5e82eb0f2f7055f1180eb6215aeb50d175aa81d0
Glitch (glitch-hello-node) <none>
2021-05-18 14:09:06 +0000

    🦃🙏 Checkpoint ./komutlar/sil2.js:31108328/238

--- komutlar/sil2.js
+++ komutlar/sil2.js
@@ -3,14 +3,15 @@ const Discord = require('discord.js');
 exports.run = function(client, message, args) {
 
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send(`Bu komutu kullanman için \`Mesajları Yönet\` Yetkisine sahip olan gerek!`);
-  
+  var must = args[0]
+  if(!must) return message.channel.send("Geçersiz argüman! `mustsil <kullanıcı> <sayı>`")
   var u = message.mentions.users.first()
   var x = args[1]
-  if (!u) return message.channel.send("Geçersiz argüman! `sil-üye <kullanıcı> <sayı>`")
+  if (!u) return message.channel.send("Geçersiz argüman! `mustsil <kullanıcı> <sayı>`")
   
-  if (!x) return message.channel.send("Geçersiz argüman! `sil-üye <kullanıcı> <sayı>`")
+  if (!x) return message.channel.send("Geçersiz argüman! `mustsil <kullanıcı> <sayı>`")
   
-  if (isNaN(x)) return message.channel.send("Geçersiz argüman! `sil-üye <kullanıcı> <sayı>`")
+  if (isNaN(x)) return message.channel.send("Geçersiz argüman! `mustsil <kullanıcı> <sayı>`")
   
   if (x < 1) return message.channel.send("``1`` adetten az mesaj silemem!")
   
@@ -40,7 +41,7 @@ exports.conf = {
 };
 
 exports.help = {
-  name: 'sil2', 
+  name: 'mustsil', 
   description: 'Slots oyunu oynatır',
-  usage: 'sil2'
+  usage: 'mustsil'
 };
