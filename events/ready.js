d9ae336d33372c4c45a62f4c893e789090fb944b
Glitch (glitch-hello-node) <none>
2021-05-23 19:35:15 +0000

    🚐📡 Checkpoint ./events/ready.js:31108328/498

--- events/ready.js
+++ events/ready.js
@@ -8,14 +8,16 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("online");
-   var oyun = [
-        "⍢ B I T C H E S",
-        "⍢ B I T C H E S"
-    ];
+  
+  var oyun = [
+    "'Must#5026",
+ 
 
-    setInterval(function() {
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-        client.user.setActivity(oyun[random], "WATCHING");
-        }, 2 * 2500);
+  ];
+
+  setInterval(function() {
+    var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
+
+    client.user.setActivity(oyun[random], "");
+  }, 2 * 2500);
 }
