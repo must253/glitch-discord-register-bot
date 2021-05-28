997a42ee7b0886950143cd0b4d65796ec3eb4c03
Glitch (glitch-hello-node) <none>
2021-05-28 18:51:46 +0000

    💇⛽️ Checkpoint ./komutlar/renk.js:31108328/2255 ./komutlar/wasted.js:31108328/436

--- komutlar/renk.js
+++ komutlar/renk.js
@@ -2,32 +2,40 @@ var Jimp = require('jimp');
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+  
+const lgbt = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Pride_Flag.svg/292px-Gay_Pride_Flag.svg.png'
 
+     var user = message.mentions.users.first() || message.author;
       message.channel.startTyping();
-        var user = 'https://cdn.glitch.com/f3d0d588-f959-4b76-93d1-649eb40e3dc7%2F48e56ab8-2892-4235-90ae-f93dbd56c5d2.image.png?v=1622226309012'
+        var user = message.mentions.users.first() || message.author;
         if (!message.guild) user = message.author;
 
-        Jimp.read(user, (err, image) => {
+        Jimp.read(user.avatarURL, (err, image) => {
             image.resize(295, 295)
-          image.color(['#ffff00']).write(`./renkk/${user.id}.png`);
+          image.blur(3)
+            Jimp.read(lgbt), (err, avatar) => {
+               avatar.resize(295, 295)
+                image.composite(avatar, 4, 0)
+              .write(`./renkk/${user.id}.png`);
           setTimeout(function() {
                     message.channel.send(new Discord.Attachment(`./renkk/${user.id}.png`));
                 }, 1000);
           message.channel.stopTyping();
+              };
         });
     };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["renk","renk"],
+  aliases: ["bulanık","bulanık"],
   permLevel: 0,
 
   kategori: "efekt"
 };
 
 exports.help = {
-  name: 'renk',
+  name: 'bulanık',
   description: 'Avatarınıza Wasted efekti verir.',
-  usage: 'renk <@kullanıcı>'
+  usage: 'bulanık <@kullanıcı>'
 };
