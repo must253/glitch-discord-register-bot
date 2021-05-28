04d94f5409b3108ff3ad1ab0f9c51e445c42f7fe
Glitch (glitch-hello-node) <none>
2021-05-28 19:02:52 +0000

    🏫🐫 Checkpoint ./komutlar/renk.js:31108328/14

--- komutlar/renk.js
+++ komutlar/renk.js
@@ -15,8 +15,7 @@ const lgbt = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Gay_Prid
           image.blur(3)
             Jimp.read(lgbt), (err, avatar) => {
                avatar.resize(295, 295)
-                image.composite(avatar, 4, 0)
-              .write(`./renkk/${user.id}.png`);
+                image.composite(avatar, 4, 0) .write(`./renkk/${user.id}.png`);
           setTimeout(function() {
                     message.channel.send(new Discord.Attachment(`./renkk/${user.id}.png`));
                 }, 1000);
