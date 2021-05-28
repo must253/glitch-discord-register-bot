e1a59e96382bdfb2bd7827c47ec65683bf6ec93a
Glitch (glitch-hello-node) <none>
2021-05-28 18:28:59 +0000

    💝😂 Checkpoint ./komutlar/renk.js:31108328/651 ./.glitch-assets:31108328/467

--- komutlar/renk.js
+++ komutlar/renk.js
@@ -3,14 +3,13 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-  var user = message.mentions.users.first() || message.author;
       message.channel.startTyping();
-        var user = message.mentions.users.first() || message.author;
+        var user = 'https://cdn.glitch.com/f3d0d588-f959-4b76-93d1-649eb40e3dc7%2F48e56ab8-2892-4235-90ae-f93dbd56c5d2.image.png?v=1622226309012'
         if (!message.guild) user = message.author;
 
-        Jimp.read(user.avatarURL, (err, image) => {
-            image.resize(295, 295).write(`./renkk/${user.id}.png`);
-          image.color([{ apply: 'red', params: ['#ff0000'] }]);
+        Jimp.read(user, (err, image) => {
+            image.resize(295, 295)
+          image.color([{ apply: 'xor', params: ['#ffff00'] }]).write(`./renkk/${user.id}.png`);
           setTimeout(function() {
                     message.channel.send(new Discord.Attachment(`./renkk/${user.id}.png`));
                 }, 1000);
