32dc84a28d1527806aa390ee61976f5c848ac0a0
Glitch (whispering-handy-crayon) <none>
2021-06-01 19:09:11 +0000

    🙌🐁 Checkpoint ./komutlar/avatar.js:31108328/1114

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -3,10 +3,11 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 
-  var user = message.mentions.users.first() || message.guild.members.get(args[0]) || message.author ;
+   var user2 = message.mentions.users.first() || message.guild.members.get(args[0]) || message.author ;
+  
+   const pp = `https://cdn.discordapp.com/avatars/${user2.id}/${user2.avatar}.png`;
       message.channel.startTyping();
         var user = message.mentions.users.first() || message.guild.members.get(args[0]) || message.author;
-  
         if (!message.guild) user = message.author;
 
         Jimp.read(user.avatarURL, (err, image) => {
