fb9c32652b775edc0c2712e5f259039822e0a1d5
Glitch (whispering-handy-crayon) <none>
2021-06-03 16:32:25 +0000

    🏀🚂 Checkpoint ./komutlar/anskm.js:31108328/367

---
+++ komutlar/anskm.js
@@ -0,0 +1,19 @@
+exports.run = async (client, message, args) => {
+  
+  
+   member.addRole('842418432980549635')  
+
+  
+  };
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['kadın'],
+  permLevel: 0
+}
+exports.help = {
+  name: 'k',
+  description: "Kadın kullanıcıları kayıt etme komutu.",
+  usage: 'kadın @kişi isim yaş'
+}
