4d7f6cc7ef409cc5f867c034779d1ab11bb93131
Glitch (whispering-handy-crayon) <none>
2021-06-27 12:52:50 +0000

    🔔🎯 Checkpoint ./package.json:31108328/35 ./codearius.js:31108328/36

--- codearius.js
+++ codearius.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-require('discord-buttons')(client);
 const ayarlar = require('./ayarlar.json');
 const chalk = require('chalk');
 const moment = require('moment');
